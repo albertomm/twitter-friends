0ec1cb89aa15ae9321e01bc6534eb754028544e5
Alberto Moreno <albertomm@gmail.com>
2016-05-24 02:28:23 +0200

    New integration tests

---
+++ test/integration/friends_integration_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class FriendsIntegrationTest < ActionDispatch::IntegrationTest
+
+  test "add one friend" do
+    # Create a user
+    username = generate_random_username
+    post '/users', {:name => username}
+    assert_response :created
+
+    # Follow a friend
+    friendname = generate_random_username
+    post "/users/#{username}/friends", {:names => friendname}
+
+    # We are redirected to the new friend resource URL
+    assert_created_redirect "/users/#{username}/friends/#{friendname}"
+    follow_created_redirect
+    assert_response :success
+  end
+
+  test "add and remove friends" do
+    # Create a user
+    username = generate_random_username
+    post_via_redirect '/users', name: username
+    assert_response :success
+
+    # Follow some friends
+    friendnames = (0..5).map { generate_random_username }
+    post "/users/#{username}/friends", names: friendnames
+    assert_response :created
+
+    # The friend list now includes the new friends
+    get "/users/#{username}/friends"
+    response_json = ActiveSupport::JSON.decode(response.body)
+    response_names = response_json.map {|f| f.fetch("name", nil) }
+    assert_equal friendnames, response_names
+
+    # Unfollow a friend
+    delete "/users/#{username}/friends/#{friendnames[2]}"
+
+    # The friend list shouldn't include the new friends
+    get "/users/#{username}/friends/#{friendnames[2]}"
+    assert_response :missing
+  end
+
+  test "users get JSON recommendations" do
+    expected_result = create_test_users
+    get '/users/X/recommendations'
+    assert_response :success
+    expected_body = ActiveSupport::JSON.encode(expected_result)
+    assert_equal expected_body, response.body
+  end
+end
---
+++ test/integration/users_integration_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UserIntegrationTest < ActionDispatch::IntegrationTest
+
+  test "get missing user returns 404" do
+    get "/users/#{generate_random_username}"
+    assert_response :missing
+  end
+
+  test "create a user by posting the name" do
+    # Post a new user name
+    username = generate_random_username
+    post "/users", name: username
+
+    # The response sould be a redirect to the User's URL
+    assert_created_redirect "/users/#{username}"
+    follow_created_redirect
+    assert_response :success
+
+    # The User's URL should display the username
+    json = ActiveSupport::JSON.decode(response.body)
+    assert json.key?('name'), "Response doesn't contain a name."
+    assert_equal json.fetch('name'), username
+  end
+
+end
