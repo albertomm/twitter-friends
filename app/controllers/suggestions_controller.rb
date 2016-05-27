d3e620063af3abbfaa097e5542d24538fe4b3463
Alberto Moreno <albertomm@gmail.com>
2016-05-27 09:36:04 +0200

    Fix: remove duplicated code in SuggestionsController

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -4,8 +4,7 @@ class SuggestionsController < ApplicationController
 
   # Get friend suggestions for the user
   def show
-    user = User.find_by!(name: params[:user_name])
-    render json: user.suggest_friends
+    render json: @user.suggest_friends
   end
 
 end
